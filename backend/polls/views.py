73f7a5dde541c15469a6cb3513ac3fd7d7d06b05
Youness <younessbilgui007@gmail.com>
2025-08-10 12:28:29 +0100

    fix: Format remaining files with Black

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -13,8 +13,12 @@ from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 from .models import Poll, PollOption, Vote
-from .serializers import (PollCreateSerializer, PollResultSerializer,
-                          PollSerializer, VoteSerializer)
+from .serializers import (
+    PollCreateSerializer,
+    PollResultSerializer,
+    PollSerializer,
+    VoteSerializer,
+)
 
 
 # Custom throttle classes for specific operations
